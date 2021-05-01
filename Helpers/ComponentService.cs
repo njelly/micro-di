b1c92241f526b341e26f43c0d229b8fc554dde04
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2021-05-01 08:45:59 -0600

    structured project directory for adding to package mananger with git url

---
+++ Helpers/ComponentService.cs
@@ -0,0 +1,29 @@
+﻿using MicroDI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace MicroDI.Helpers
+{
+    public class ComponentService
+    {
+        private static Dictionary<Type, IEnumerable<MemberInfo>> _cachedPathInjectMembers = new Dictionary<Type, IEnumerable<MemberInfo>>();
+
+        private readonly IContainer _container;
+
+        public ComponentService(IContainer container)
+        {
+            _container = container;
+        }
+
+        public void Inject(Component component)
+        {
+            if (component == null)
+                return;
+
+            // Injects [Inject]...
+            _container.Injector.InjectMembers(component);
+        }
+    }
+}
--- MicroDI/Assets/MicroDI/Interfaces/IFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using MicroDI.Interfaces;
-
-namespace Assets.MicroDI.Interfaces
-{
-    public interface IFactory
-    {
-        object Create(IContainer container, IBinding binding, params object[] args);
-    }
-}
